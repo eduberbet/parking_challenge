cd2e4fbabbd9a692facf909b8ba2767688572d82
eduberbet <edu.berbet@hotmail.com>
2021-03-12 20:42:23 -0300

    Standalone terminado

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Parking Challenge
+# Parking Challenge
 
 # Sobre
 
@@ -9,7 +9,25 @@ A aplicação consiste em uma web app para registro e visualização de entradas
  - JavaScript;
  - HTML5;
  - CSS3.
+ - Node v15.11.0
 
 # Run code, run...
 
+## Modo simples
+
 Baixe os arquivos e abra o index.html
+
+## Modo com servidor node
+
+ - Baixe todos os arquivos;
+ - Crie uma pasta com o nome "Parking Challenge" (sem aspas);
+ - Coloque todos os arquivos dentro desta pasta exceto o server.js;
+ - Via PS (powershell) ou bash chegue na pasta superior (onde via ls (bash) ou dir (PS) veja a pasta e o arquivo server.js)
+ - Execute o comando: npm init -y 
+ - Execute o comando: npm i express
+ - Execute o comando: node server.js
+ - Abra o navegador o endereço: localhost:8000
+
+# Observações 
+ - Para mudar a porta do servidor basta abrir o arquivo server.js e trocar o valor 8000
+ - Web App Standalone, todo o Banco de dados fica armazenado em BDRAM.js logo ao desligar o servidor todos os dados são perdidos.
--- static/js/BDRAM.js
+++ static/js/BDRAM.js
@@ -1,29 +1,47 @@
-let Limite = 40
+let Limite = 40 //Limite arbitrario de vagas no estacionamento
 let ocupação = 0
-let veiculo = []
-let minutos = []
-let pagamento = []
-let His_veiculo = []
-let His_time = []
-let His_day = []
+let veiculo = [] //Array com os veiculos estacionados
+let minutos = [] //Array com o horario (em minutos) dos veiculos estacionados
+let pagamento = [] //Array booleano do status de pagamento
+let His_veiculo = [] //Arquiva veiculos que pagaram o estacionamento
+let His_time = [] //Arquiva o tempo que veiculos ficaram no estacionamento
+let His_day = [] //Arquiva o dia que o veiculo ficou estacionado
+
+function limpar(id) { //limpa o campo de input e deixa o cursor lá
+    id.value = ''
+    id.focus()
+}
+
+function PeriodoEstacionado(id_veiculo) { //recebe o id do veiculo e retorna string formatada com o tempo estacionado.
+    let data = new Date()
+    let hora = data.getHours()
+    let min = data.getMinutes()
+    let now = Number(hora) * 60 + Number(min)
+    let periodo = now - minutos[id_veiculo]
+    let horas = Math.floor(periodo / 60)
+    let minut = periodo % 60
+    let periodo_estacionado = horas + "h" + minut + "min"
+    return (periodo_estacionado)
+}
+
+function Placa(id) {
+    let temp = id.value
+    let placa = temp.toUpperCase()
+    return placa
+}
 
 function ENTRADA() {
     let id = document.querySelector("input#eplaca")
-    let temp = id.value
-    let placa = temp.toUpperCase()
-
+    let placa = Placa(id)
     if (ocupação == Limite) {
         window.alert("Estacionamento Lotado!")
-        id.value = ''
-        id.focus()
+        limpar(id)
     } else if (placa.length > 8 || placa.length < 7) {
         window.alert("[ERRO] Padrão de placa inválido!")
-        id.value = ''
-        id.focus()
+        limpar(id)
     } else if (veiculo.indexOf(placa) !== -1) {
         window.alert("Veiculo já foi registrado!")
-        id.value = ''
-        id.focus()
+        limpar(id)
     } else {
         let data = new Date()
         let hora = data.getHours()
@@ -32,8 +50,7 @@ function ENTRADA() {
         minutos.push(((Number(hora) * 60) + Number(min)))
         pagamento.push(false)
         ocupação++
-        id.value = ''
-        id.focus()
+        limpar(id)
         window.alert("REGISTRADO!")
     }
 
@@ -41,80 +58,62 @@ function ENTRADA() {
 
 function PAGAMENTO() {
     let id = document.querySelector("input#splaca")
-    let temp = id.value
-    let placa = temp.toUpperCase()
+    let placa = Placa(id)
 
     if (placa.length > 8 || placa.length < 7) {
         window.alert("[ERRO] Padrão de placa inválido!")
-        id.value = ''
-        id.focus()
+        limpar(id)
     } else if (veiculo.indexOf(placa) == -1) {
         window.alert("[ERRO] Veiculo não foi registrado!")
-        id.value = ''
-        id.focus()
+        limpar(id)
     } else {
         let id_veiculo = veiculo.indexOf(placa)
         if (pagamento[id_veiculo] == false) {
             let data = new Date()
             let day = data.getDay() + "/" + data.getMonth()
-            let hora = data.getHours()
-            let min = data.getMinutes()
-            let saida = Number(hora) * 60 + Number(min)
-            let periodo = saida - minutos[id_veiculo]
-            let horas = Math.floor(periodo / 60)
-            let minut = periodo % 60
-            let periodo_estacionado = horas + "h" + minut + "min"
+            let periodo_estacionado = PeriodoEstacionado(id_veiculo)
             let confirm = window.confirm(`Confirma o pagamento de ${periodo_estacionado} do veículo ${placa}?`)
             if (confirm == false) {
-                id.value = ''
-                id.focus()
+                limpar(id)
             } else {
                 His_veiculo.push(placa)
                 His_time.push(periodo_estacionado)
                 His_day.push(day)
                 pagamento[id_veiculo] = true
                 window.alert("PAGAMENTO CONFIRMADO!")
-                id.value = ''
-                id.focus()
+                limpar(id)
             }
         } else {
             window.alert("[ERRO] Veículo já realizou o pagamento!")
-            id.value = ''
-            id.focus()
+            limpar(id)
         }
     }
 }
 
 function SAIDA() {
     let id = document.querySelector("input#splaca")
-    let temp = id.value
-    let placa = temp.toUpperCase()
+    let placa = Placa(id)
 
     if (placa.length > 8 || placa.length < 7) {
         window.alert("[ERRO] Padrão de placa inválido!")
-        id.value = ''
-        id.focus()
+        limpar(id)
     } else if (veiculo.indexOf(placa) == -1) {
         window.alert("[ERRO] Veiculo não foi registrado!")
-        id.value = ''
-        id.focus()
+        limpar(id)
     } else {
         let id_veiculo = veiculo.indexOf(placa)
         if (pagamento[id_veiculo] == false) {
             window.alert("[ERRO] Veículo não realizou o pagamento!")
-            id.value = ''
-            id.focus()
+            limpar(id)
         } else {
             let confirmacao = window.confirm(`Confirmar a saida do veículo ${placa}?`)
             if (confirmacao == false) {
-                id.value = ''
-                id.focus()
+                limpar(id)
             } else {
                 veiculo[id_veiculo] = ''
                 ocupação--
                 window.alert("SAIDA LIBERADA!")
-                id.value = ''
-                id.focus()
+                limpar(id)
             }
         }
     }
@@ -122,37 +121,24 @@ function SAIDA() {
 
 function HISTORICO() {
     let id = document.querySelector("input#hplaca")
-    let temp = id.value
-    let placa = temp.toUpperCase()
+    let placa = Placa(id)
     let historico = document.getElementById("his")
-    historico.innerHTML = `<p>${His_veiculo}</p>`
 
     if (placa.length > 8 || placa.length < 7) {
         window.alert("[ERRO] Padrão de placa inválido!")
-        id.value = ''
-        id.focus()
+        limpar(id)
     } else if (His_veiculo.indexOf(placa) == -1 || veiculo.indexOf(placa) == -1) {
         window.alert("[ERRO] Veiculo não encontrado!")
-        id.value = ''
-        id.focus()
+        limpar(id)
     } else {
-        id.value = ''
-        id.focus()
+        limpar(id)
         historico.innerHTML = ``
         historico.innerHTML = `<p> </p>`
         historico.innerHTML += `Placa: ${placa}`
         historico.innerHTML += `<p> </p>`
         for (var pos in veiculo) {
             if (veiculo[pos] == placa) {
-                let id_veiculo = veiculo.indexOf(placa)
-                let data = new Date()
-                let hora = data.getHours()
-                let min = data.getMinutes()
-                let now = Number(hora) * 60 + Number(min)
-                let periodo = now - minutos[id_veiculo]
-                let horas = Math.floor(periodo / 60)
-                let minut = periodo % 60
-                let periodo_estacionado = horas + "h" + minut + "min"
+                let periodo_estacionado = PeriodoEstacionado(pos)
                 historico.innerHTML += `<p>Tempo Atual: ${periodo_estacionado}</p>`
             }
         }
